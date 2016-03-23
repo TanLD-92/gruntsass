7e755b894000ec6e0e4592933de39027582c3337
tanld <tanld@greenglobal.vn>
2016-03-24 01:18:06 +0700

    update

---
+++ WebGruntDemo1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebGruntDemo1.Startup))]
+namespace WebGruntDemo1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ WebGruntDemo1/gruntfile.js
@@ -0,0 +1,27 @@
+﻿module.exports = function (grunt) {
+    grunt.initConfig({
+        // Watch task config
+        watch: {
+            options: {
+                livereload: true
+            },
+            sass: {
+                files: "Styles/scss/**/*.scss",
+                tasks: ['sass']
+            }
+        },
+        // SASS task config
+        sass: {
+            dev: {
+                files: {
+                    // destination         // source file
+                    "Content/main.css": "Styles/scss/**/*.scss"
+                }
+            }
+        },
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', ['watch']);
+};
